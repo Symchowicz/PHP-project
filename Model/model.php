411ae8e094238580eef94e15e2df19148846303d
Symchowicz <symchowiczbenji@gmail.com>
2021-12-06 15:21:18 +0100

    Debut mise en place MVC

---
+++ Model/model.php
@@ -0,0 +1,39 @@
+<?php
+
+
+    class PostManager extends BaseManager
+    {
+        /**
+        * @param int|null$number
+        * @return array
+        */
+        public function getPosts(int $number = null): array
+        {
+            if ($number) {
+                $query = $this->db->prepare('SELECT * FROM posts ORDER BY id DESC LIMIT :limit');
+                $query->bindValue(':limit',$number,\PDO::PARAM_INT);
+                $query->execute();
+            } else {
+                $query = $this->db->query(' SELECT * FROM posts ORDER BY id DESC');
+            }
+            $query->setFetchMode(\PDO::FETCH_CLASS|\PDO::FETCH_PROPS_LATE,
+            'Entity\Post');
+
+            return$query->fetchAll();
+        }
+
+        /**
+        * @param int $id
+        * @return Post|bool
+        */
+        public function getPostById(int $id)
+        {
+            $query = $this->db->prepare('SELECT* FROM posts WHERE id = :id') ;
+            $query->bindValue(':id',$id,\PDO::PARAM_INT);
+            $query->execute();
+            $query->setFetchMode(\PDO::FETCH_CLASS|\PDO::FETCH_PROPS_LATE,'Entity\Post');
+            return$query->fetch();
+        }
+    }
+
+?>
