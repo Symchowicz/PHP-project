196794d87436dae436bae6fb2200b40d19ddea20
PiKwaii93 <maxence.nho@hetic.net>
2021-12-09 10:59:53 +0100

    sql

--- app/index.php
+++ app/index.php
@@ -1,7 +1,7 @@
 <?php
     // Load Config
-    require_once '../config/config.php';
-    require_once '../vendor/autoload.php';
+    require_once './src/config/factories.php';
+    require_once './vendor/autoload.php';
     $router = new App\routes\Router() ;
     $router->getController();
 ?>
--- app/src/config/factories.php
+++ app/src/config/factories.php
@@ -2,13 +2,57 @@
     $user = 'root';
     $pass = 'example';
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=CMS', $user, $pass);
-        foreach($dbh->query('SELECT * from *') as $row) {
-            print_r($row);
-        }
+        $dbh = new PDO('mysql:host=db;dbname=CMS', $user, $pass);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbh->query("SELECT * FROM users_table");
+        print_r($row);
         $dbh = null;
     } catch (PDOException $e) {
         print "Erreur !: " . $e->getMessage() . "<br/>";
+        echo("Miaou");
         die();
     }
+?>
+
+<?php 
+
+$dsn = "mysql:host=db";
+$user = "root";
+$pwd = "example";
+$pdo = new PDO($dsn, $user, $pwd);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+/*echo("miaou");
+echo("<br>");
+
+$requete = $pdo->query("SELECT * FROM `CMS`.`users_table`");
+
+
+echo("miaou");
+echo("<br>");
+
+while($data = $requete->fetch()){
+
+    echo($data["users_id"]);
+    echo("<br>");
+    echo($data["first_name"]);
+    echo("<br>");
+    echo($data["last_name"]);
+    echo("<br>");
+    echo($data["email"]);
+    echo("<br>");
+    echo($data["is_admin"]);
+    echo("<br>");
+    echo($data["password"]);
+    echo("<br>");
+    echo("miaou");
+    echo("<br>");
+
+}
+
+echo("miaou");
+echo("<br>");*/
+
 ?>
